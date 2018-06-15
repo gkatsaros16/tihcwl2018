dc09d7f25fbb8d1a2377a860d8a3c25b232b9007
George <gkatsaros16@gmail.com>
2018-06-15 05:47:50 +0100

    added router, started on edit wishlist pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/navbar/navbar.component';
@@ -7,6 +8,23 @@ import { FooterComponent } from './components/footer/footer.component';
 import { CardComponent } from './components/card/card.component';
 import { TopFourComponent } from './components/topFour/topFour.component';
 import { MainWishListComponent } from './components/mainWishList/mainWishList.component';
+import { HomeComponent } from './components/home/home.component';
+import { EditWishListComponent } from './components/editWishList/editWishList.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'editWishList', component: EditWishListComponent },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  // { path: '',
+  //   redirectTo: '/heroes',
+  //   pathMatch: 'full'
+  // },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -15,10 +33,16 @@ import { MainWishListComponent } from './components/mainWishList/mainWishList.co
     NavBarComponent,
     CardComponent,
     TopFourComponent,
-    MainWishListComponent
+    MainWishListComponent,
+    HomeComponent,
+    EditWishListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/editWishList/editWishList.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-wish-list',
+  templateUrl: './editWishList.component.html',
+  styleUrls: ['./editWishList.component.css']
+})
+export class EditWishListComponent {
+  wishList = ['']
+}
